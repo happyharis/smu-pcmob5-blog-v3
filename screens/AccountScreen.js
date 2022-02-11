a6ff71d278b09f0dc19c27799eeafb3a0aea9936
Haris Samingan <muhamad_haris@hotmail.com>
2022-02-11 18:10:44 +0800

    Resolve api url

--- screens/AccountScreen.js
+++ screens/AccountScreen.js
@@ -4,7 +4,7 @@ import { commonStyles } from "../styles/commonStyles";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 
-const API = "https://yjsoon.pythonanywhere.com";
+const API = "https://thehappyharis.pythonanywhere.com";
 const API_WHOAMI = "/whoami";
 
 export default function AccountScreen({ navigation }) {
